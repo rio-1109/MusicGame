04b7dde0d241e5980acefb986048fc11cce3338e
rio-1109 <g19928nr@gm.tsuda.ac.jp>
2021-11-13 03:03:02 +0900

    mp3の送信

--- Assets/Scripts/Connect.cs
+++ Assets/Scripts/Connect.cs
@@ -3,33 +3,37 @@ using UnityEngine.Networking;
 using UnityEngine;
 using System.IO;
 using UnityEngine.SceneManagement;
+using static System.IO.Path;
 
 public class Connect : MonoBehaviour {
     // Start is called before the first frame update
     string URL = "https://music-game-331007.an.r.appspot.com";
     public string chartsPath = "Assets/Charts/test1.json";
+    public string path;
+    public string title;
     
      public void  startConnect(string fileBrowserResult) {
-        string path = fileBrowserResult;
+        path = fileBrowserResult;
         Debug.Log(path);
+        title = Path.GetFileNameWithoutExtension(path);
+        Debug.Log(title);
         StartCoroutine("OnSend", URL);
         
     }
 
      IEnumerator OnSend(string url) {
-         Debug.Log("!!!!");
-        UnityWebRequest webRequest = UnityWebRequest.Get(url);
-        Debug.Log("??????");
-        yield return webRequest.SendWebRequest();
-        Debug.Log("~~~~~~~");
-
-        if (webRequest.result == UnityWebRequest.Result.ConnectionError) {
-            Debug.Log(webRequest.error);
-        } else {
-            Debug.Log("Get"+" : "+ webRequest.downloadHandler.text);
-            File.WriteAllText(chartsPath, webRequest.downloadHandler.text);
-
-        }
+         WWWForm form = new WWWForm();
+         form.AddField("title","hello");
+         byte[] bytes = File.ReadAllBytes(path);
+         form.AddBinaryData("mp3", bytes, Path.GetFileName(path),"multipart/form-data");
+         UnityWebRequest webRequest = UnityWebRequest.Post(url, form);
+         yield return webRequest.SendWebRequest();
+         if (webRequest.result == UnityWebRequest.Result.ConnectionError) {
+             Debug.Log(webRequest.error);
+            } else {
+                 Debug.Log("Get"+" : "+ webRequest.downloadHandler.text);
+                 File.WriteAllText(chartsPath, webRequest.downloadHandler.text);
+            }
         SceneManager.LoadScene("Scene2");
     }
 }
